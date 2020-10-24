99e17e3aafb8f3c8248b434645244541cbe09ca5
Guilherme <marquesguil@gmail.com>
2020-10-24 14:32:06 -0300

    Criando projeto Spring MVC JPA e Hibernate com lnacamentos

---
+++ src/main/java/com/curso/exeLancamentos/ExeLancamentosApplication.java
@@ -0,0 +1,13 @@
+package com.curso.exeLancamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExeLancamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExeLancamentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/curso/exeLancamentos/ExeLancamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.exeLancamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExeLancamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
